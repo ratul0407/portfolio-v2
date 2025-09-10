18b915da6b0b03bf4021d130da613abbee601eef
ratul0407 <rajaulislamratul12@gmail.com>
2025-09-10 19:24:17 +0600

    fixed some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,9 @@ function App() {
         <div
           className="absolute inset-0 z-0"
           style={{
-            backgroundImage: `radial-gradient(circle 500px at 50% 300px, rgba(16,185,129,0.35), transparent)`,
+            backgroundImage: `radial-gradient(circle 500px at 50% ${
+              window.innerHeight / 2 - 50
+            }px, rgba(16,185,129,0.35), transparent)`,
           }}
         />
         {/* Your Content/Components */}
--- src/components/BlogsComponent.tsx
+++ src/components/BlogsComponent.tsx
@@ -4,8 +4,10 @@ import BlogPreview from "./BlogsPreview";
 export default function BlogsComponent() {
   return (
     <section className="py-16 px-6 md:px-20 bg-black text-white">
-      <h2 className="text-3xl md:text-4xl font-bold mb-8">Latest Blogs</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <h2 className="text-3xl md:text-4xl font-bold mb-8  text-center">
+        Latest Blogs
+      </h2>
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
         {blogsData.map((blog) => (
           <BlogPreview key={blog.id} {...blog} />
         ))}
